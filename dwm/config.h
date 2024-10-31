671fda570a85d0b6cbe1e651b9e864354d1cbe5e
Tahsin404 <tahsin.j.tanveer@gmail.com>
2024-10-31 12:58:09 +0600

    come minor changes

--- dwm/config.h
+++ dwm/config.h
@@ -9,9 +9,9 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const int vertpad            = 20;       /* vertical padding of bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 35;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Monofur Nerd Font Mono:style=Bold:size=21" };
 static const char dmenufont[]       = "Monofur Nerd Font Mono:style=Bold:size=15";
 static const char col_gray1[]       = "#9064e3";
