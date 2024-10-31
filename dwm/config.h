91b7160c4966c06afcabf0159cb9ccecedb1fc3e
Tahsin404 <tahsin.j.tanveer@gmail.com>
2024-10-31 08:07:13 +0600

    Replaced generic bar with polybar
    Download specific polybar-dwm-module fork to make this work

--- dwm/config.h
+++ dwm/config.h
@@ -1,66 +1,45 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 14;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const int vertpad            = 15;       /* vertical padding of bar */
-static const int sidepad            = 13;       /* horizontal padding of bar */
-static const int user_bh            = 32;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Lekton Nerd Font:style=Regular:size=17" };
-static const char dmenufont[]       = "Lekton Nerd Font:style=Regular:size=14";
-static const char norm_fg[]         = "#ac81c1";
-static const char norm_bg[]         = "#05010A";
-static const char norm_border[]     = "#785a87";
-static const char sel_fg[]          = "#ac81c1";
-static const char sel_bg[]          = "#3B2054";
-static const char sel_border[]      = "#ac81c1";
-static const char urg_fg[]          = "#ac81c1";
-static const char urg_bg[]          = "#361549";
-static const char urg_border[]      = "#361549";
-static const char col_bg[]          = "#000000";
-static const char col_gray2[]       = "#f562f5";
+static const int vertpad            = 20;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const char *fonts[]          = { "Monofur Nerd Font Mono:style=Bold:size=21" };
+static const char dmenufont[]       = "Monofur Nerd Font Mono:style=Bold:size=15";
+static const char col_gray1[]       = "#9064e3";
+static const char col_gray2[]       = "#c2a6f7";
 static const char col_gray3[]       = "#c2a6f7";
 static const char col_gray4[]       = "#9064e3";
-static const char col_gray5[]       = "#f7055e";
-static const char col_gray6[]       = "#03f7ff";
-static const char col_gray7[]       = "#f4fc05";
-static const char col_gray8[]       = "#e265eb";
-static const char col_gray9[]       = "#f702e7";
 static const char col_cyan[]        = "#9daafa";
-static const unsigned int baralpha = 0x00;
+static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */          /* Hot Pink Theme*/ 
-	[SchemeNorm] = { col_gray3, norm_bg, col_gray4 },
-	[SchemeSel]  = { col_gray4, col_gray3,  col_gray2  },
-
-	/*               fg         bg         border   */         /* Cyberpunk Theme */
-	/*[SchemeNorm] = { col_gray5, col_bg, col_gray8 },
-	[SchemeSel]  = { col_gray6, col_gray7,  col_gray9  },*/
-
-  /*[SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins*/
-  /*[SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win*/
-
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
 };
 
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
     [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, OPAQUE, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 static const char *const autostart[] = {
+	"slstatus", NULL,
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { " ", " ", "󰈹 ", "󰣇 ", "󰓇 ", " " };
+static const char *tags[] = { "  ", "  ", "  ", " 󰣇 ", "  ", "  " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -69,13 +48,13 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
@@ -99,7 +78,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
@@ -110,12 +89,12 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } }, 
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -128,7 +107,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { MODKEY|ShiftMask,              XK_y, spawn,            SHCMD("deepin-screen-recorder") },
+  { MODKEY|ShiftMask,              XK_y, spawn,            SHCMD("flameshot gui") },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
